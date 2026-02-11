810e15627f0c7de1fe0aa646e226737f6b6240d1
sayemhaque <mdsayemofficial1000@gmail.com>
2026-02-11 15:48:27 +0600

    add changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portflio-sayem",
+  trailingSlash: true,
 };
 
 export default nextConfig;
