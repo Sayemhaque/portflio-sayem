c73372ccf7a507735438b50ae135a7cca1591e56
sayemhaque <mdsayemofficial1000@gmail.com>
2026-02-11 15:32:24 +0600

    Updates

--- _next/static/LVs_sZmarHNBgCsDTfWD1/_buildManifest.js
+++ _next/static/LVs_sZmarHNBgCsDTfWD1/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portflio-sayem//_next/:path+",
-        "destination": "/portflio-sayem/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
